945b788143958038b36b4ba72f124d156a8d0014
vincilisi <lisitano95@gmail.com>
2025-05-25 18:24:20 +0200

    Varabili

---
+++ main1.js
@@ -0,0 +1,8 @@
+let num1 = 12;
+let num2 = 8;
+console.log(num1, num2); // Output: 12 8
+let string = "Hello, World!";
+console.log(string); // Output: Hello, World!
+const PIGRECO = 3.14159265359;
+console.log(PIGRECO); // Output: 3.14159265359
+// const PIGRECO= 3.14; // This will cause an error because PIGRECO is a constant and cannot be reassigned
